be0e808d98e25a0dd095fc3960f8cc1badba5cdc
kolibri383 <dima.dima.16173@gmail.com>
2022-12-02 19:32:52 +0300

    создал проект

---
+++ src/main/kotlin/com/example/sqlexecutorservice/SqlExecutorServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sqlexecutorservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SqlExecutorServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SqlExecutorServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/sqlexecutorservice/SqlExecutorServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sqlexecutorservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SqlExecutorServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
